3ef2dbecebebebbae56ebdadfc1d51921f92108d
Maria Terzi <mariaterzi@iMac.localdomain>
2025-08-01 14:51:45 +0300

    push

--- app.py
+++ app.py
@@ -28,46 +28,6 @@ def home():
     return render_template('index.html', output=output)
 
 
-@app.route('/preview', methods=['POST'])
-def preview():
-    try:
-        url = request.form.get('url')
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # inject external resources into HTML
-        for link in soup.find_all('link'):
-            if link.get('href'):
-                link['href'] = urljoin(url, link['href'])
-        
-        # Uncomment this if you want to enable script
-        # for script in soup.find_all('script'):
-        #     if script.get('src'):
-        #         script['src'] = urljoin(url, script['src'])
-
-        for img in soup.find_all('img'):
-            if img.get('src'):
-                img['src'] = urljoin(url, img['src'])
-
-        return render_template('preview.html', content=soup.prettify())
-    except Exception as e:
-        return  f"Error: {e}"
-
-
-@app.route('/source-code', methods=['GET','POST'])
-def view_source_code():
-
-    try:
-        url = request.form.get('url')
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        formatted_html = soup.prettify()
-        
-        return render_template('source_code.html', formatted_html = formatted_html, url = url)
-    
-    except Exception as e:
-        return  f"Error: {e}"
-
 @app.route('/update-db')
 def update_db(): 
     try:
